f4ec316c70701b2118f7fa7f5d836fe56055a569
kaycheez <kevin.m.chau@gmail.com>
2018-08-16 16:03:28 -0700

    Refactored to react-redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
 // const enhancers = compose(
-//     applyMiddleware(thunk, apiMiddleware(options)),
+//     applyMiddleware(thunk),
 //     window.devToolsExtension ? window.devToolsExtension() : f => f
 // );
 
 
-var store = createStore(rootReducer, {videos: [], currentVideo: null}, applyMiddleware(thunk));
+var store = createStore(rootReducer, {videoList: exampleVideoData, currentVideo: exampleVideoData[0]}, applyMiddleware(thunk));
 
 export default store;
