f875951a424ef238f63c932b154c7b8468301056
kaycheez <kevin.m.chau@gmail.com>
2018-08-15 17:31:32 -0700

    Done with Action and Reducers, each have one small error

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,12 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
-  //TODO: define a reducer for the videoList field of our state.
+var videoListReducer = (state = [], action) => { // change to [] for default
+  switch (action.type) {
+    case 'CHANGE_VIDEO_LIST':
+        return action.videos;
+    default:
+        return state;
+  }      
 };
 
 export default videoListReducer;
